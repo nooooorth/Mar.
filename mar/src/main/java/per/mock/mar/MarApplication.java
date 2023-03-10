9a8b9a2ba2a57fb545de73e6fec6aafa46db516d
wjy1478 <54655031+wjy1478@users.noreply.github.com>
2023-03-10 02:06:20 +0000

    Init: init commit

---
+++ mar/src/main/java/per/mock/mar/MarApplication.java
@@ -0,0 +1,13 @@
+package per.mock.mar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarApplication.class, args);
+	}
+
+}
---
+++ mar/src/test/java/per/mock/mar/MarApplicationTests.java
@@ -0,0 +1,13 @@
+package per.mock.mar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
